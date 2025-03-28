4024b69c6ced99fdc2f1635dd73bbe1add450c3c
hyun minyoung <kingmandoo95@gmail.com>
2025-03-28 17:33:13 +0900

    git init

---
+++ src/main/java/com/lgcns/newspacenoticeservice/NewspaceNoticeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.newspacenoticeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspaceNoticeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewspaceNoticeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lgcns/newspacenoticeservice/NewspaceNoticeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgcns.newspacenoticeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewspaceNoticeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
